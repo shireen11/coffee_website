b4e24be3a78090ff848559427085acfb8a1f3527
shireen11 <85858600+shireen11@users.noreply.github.com>
2022-11-22 04:51:52 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,26 @@
 let navbar = document.querySelector('.navbar');
-
 document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
-
 let searchForm = document.querySelector('.search-form');
-
 document.querySelector('#search-btn').onclick = () =>{
     searchForm.classList.toggle('active');
     navbar.classList.remove('active');
     cartItem.classList.remove('active');
 }
-
 let cartItem = document.querySelector('.cart-items-container');
-
 document.querySelector('#cart-btn').onclick = () =>{
     cartItem.classList.toggle('active');
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
 }
-
 window.onscroll = () =>{
     navbar.classList.remove('active');
     searchForm.classList.remove('active');
     cartItem.classList.remove('active');
 }
-
 function SubmitForm() {
     try{
         Email.send({ 
@@ -51,8 +44,6 @@ function SubmitForm() {
                     console.log(err)
         }
     }
-
-
     var addItemId=0;
     function addToCart(item){
         addItemId++;
@@ -79,7 +70,6 @@ function SubmitForm() {
         delBtn.setAttribute('onclick', 'del('+addItemId+')');
         selectedItem.append(delBtn);
     }
-
     function del(item){
         document.getElementById(item).remove();
     }
